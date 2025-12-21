3ca5ec3ddfb6c5c1125f789ec555aad0c1e3bbf5
hh65 <hh65@sussex.ac.uk>
2025-12-21 10:55:29 +0000

    fix: synchronize package versions and simplify release workflow
      - Reset all package versions to 0.2.4 for synchronized versioning
      - Update commitizen config to manage all package versions
      - Simplify release.yml workflow to use single cz bump command
      - Remove complex scope-based version bumping logic

--- packages/omero-screen-plots/src/omero_screen_plots/__init__.py
+++ packages/omero-screen-plots/src/omero_screen_plots/__init__.py
@@ -6,7 +6,7 @@ The omero_screen_plots package provides plotting and analysis tools for OMERO sc
 
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.4"
 
 
 # Import user-facing plot functions
--- packages/omero-utils/src/omero_utils/__init__.py
+++ packages/omero-utils/src/omero_utils/__init__.py
@@ -13,7 +13,7 @@ Available modules:
 
 """
 
-__version__ = "0.1.1"
+__version__ = "0.2.4"
 from omero_screen.config import set_env_vars
 
 set_env_vars()
